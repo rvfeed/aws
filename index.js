f5801ca0fb6b53a2bfed55910ad2227b5b490ff9
Boddu, Raja (rb651w) <rb651w@att.com>
2018-07-19 12:53:42 +0530

    get port number from console

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const minimist = require("minimist");
 const app = express();
+const args = minimist(process.argv.slice(2));
+console.log(args)
+args.port  = args.port || 3000;
 app.get("/", (req, res) =>{
     res.send("Welcome to Node AWS");
     res.end();
 })
-app.listen(9000, () =>{
-    console.log("Node server is running on port "+9000);
+app.listen(args.port, () =>{
+    console.log("Node server is running on port "+args.port);
 })
